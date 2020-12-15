c99f52b0c4bd98afd5afacfa2158420729e58d4f
brkeozbkk <brkeozbk@gmail.com>
2020-12-15 17:22:04 +0300

    footer düzenleme

--- kan_bankasi/content/footer.php
+++ kan_bankasi/content/footer.php
@@ -3,13 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+  <link rel="stylesheet" href="css/footer.css?key=<?=time()?>">
   <title>Document</title>
 </head>
 <body>
-  <div class="footer">
-
-
-
+  <footer class="footer">
+    <div>
+     <ul>
+      <li><a href="#" class="fa fa-facebook"> </a> </li>
+     
+</ul>
 </div>
+</footer>
 </body>
 </html>
